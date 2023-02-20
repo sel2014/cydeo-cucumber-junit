2239f61833c301fe9e0fa8991019c2c45e936404
Selcuk Cellik <sel2014@hotmail.com>
2023-02-20 11:57:50 +0000

    day17 Dropdown datatable task

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -1,5 +1,6 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class WebTableLoginPage {
 
-    public WebTableLoginPage(){
+    public WebTableLoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
@@ -20,4 +21,42 @@ public class WebTableLoginPage {
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
 
+    //====================================================================
+
+
+    /**
+     * No parameters.
+     * When we call this method, it will directly login using
+     * <p>
+     * Username: Test
+     * Password: Tester
+     */
+    public void login() {
+        inputUsername.sendKeys("Test");
+        inputPassword.sendKeys("Tester");
+        loginButton.click();
+    }
+
+    /**
+     * This method will accept two arguments and login.
+     *
+     * @param username
+     * @param password
+     */
+    public void login(String username, String password) {
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+    }
+
+    /**
+     * This method will log in using credentials from
+     * configuration.properties
+     */
+    public void loginWithConfig() {
+        inputUsername.sendKeys(ConfigurationReader.getProperty("web.table.username"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("web.table.pw"));
+        loginButton.click();
+
+    }
 }
--- src/test/resources/features/webTableLogin.feature
+++ src/test/resources/features/webTableLogin.feature
@@ -8,7 +8,7 @@ Feature: User should be able to login with correct credentials
     And  user clicks to login button
     Then user should see url contains orders
 
-  @wip
+
   Scenario: Positive login scenario
     Given  user is on the login page of the app
     When  user enters  username "Test" password "Tester" and logins
